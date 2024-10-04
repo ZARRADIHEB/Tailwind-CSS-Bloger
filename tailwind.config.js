12d8ee15e19aa76d1e9404beb035e07a94e2f798
iZ17 <zarradiheb007@gmail.com>
2024-10-04 09:58:37 +0100

    first-commit

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./dist/**/*.html"],
+  theme: {
+    extend: {
+      backgroundImage:{
+'hero-bg': "url('.  /images/bg.jpg')",
+      }
+      
+    }
+  },
+  plugins: [],
+}
+
